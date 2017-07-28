9d31a2f2fbd728d66155ae99a50aaa8b703917c5
Florian Schulz <mail@getflourish.com>
2017-07-28 15:49:37 +0200

    Update Select Parent Group.js

--- Sketch Mate.sketchplugin/Contents/Sketch/Select/Select Parent Group.js
+++ Sketch Mate.sketchplugin/Contents/Sketch/Select/Select Parent Group.js
@@ -5,14 +5,24 @@ var onRun = function (context) {
     // old school variable
     doc = context.document;
     selection = context.selection;
+    var page = doc.currentPage();
 
     if (selection.count() > 0) {
         var parent = selection[0].parentGroup();
         if (parent.className() != "MSPage") {
-            doc.currentPage().deselectAllLayers();
-            parent.setIsSelected(true)
+            deselectAllLayers(page)
+            page.changeSelectionBySelectingLayers_([parent]);
         }
     } else {
         doc.showMessage("Please select a layer.");
+    }
+}
+
+
+function deselectAllLayers (page) {
+    if (page.deselectAllLayers) {
+        page.deselectAllLayers();
+    } else {
+        page.changeSelectionBySelectingLayers_([]);
     }
 }
