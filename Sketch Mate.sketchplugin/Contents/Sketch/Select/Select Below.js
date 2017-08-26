9b6fa9d5c03303f0c730f918fbff4f63f810e510
Arroyodorado <arroyodorado@gmx.de>
2017-08-26 17:22:36 +0200

    Fix for "Select Below" for Sketch 46

--- Sketch Mate.sketchplugin/Contents/Sketch/Select/Select Below.js
+++ Sketch Mate.sketchplugin/Contents/Sketch/Select/Select Below.js
@@ -4,9 +4,10 @@ var onRun = function(context) {
 
   var doc = context.document;
   var selection = context.selection[0];
+  var page = doc.currentPage();
 
   // set the scope to the layer group that the currently selected layer is in
-  var scope = selection.parentGroup().layers().array();
+  var scope = selection.parentGroup().layers();
 
   // calculate the bottom position of the selected layer
   var bottom = selection.absoluteRect().y() + selection.absoluteRect().height();
@@ -15,6 +16,25 @@ var onRun = function(context) {
   var predicate = NSPredicate.predicateWithFormat("absoluteRect.y >= %@", bottom);
   var queryResult = scope.filteredArrayUsingPredicate(predicate);
 
-  // select all layers that match the query
-  doc.currentPage().selectLayers(queryResult);
+  // code below contains code from Ashung Hung, Ashung.hung@gmail.com
+
+  // Deselect all layers
+  if (page.deselectAllLayers) {
+      page.deselectAllLayers();
+  } else {
+      page.changeSelectionBySelectingLayers(nil);
+  }
+
+  var loop = queryResult.objectEnumerator();
+  var layer;
+  while (layer = loop.nextObject()) {
+      if (layer.parentGroup().class() != "MSPage") {
+          // Fix Sketch 45
+          if (MSApplicationMetadata.metadata().appVersion < 45) {
+              layer.select_byExpandingSelection(true, true);
+          } else {
+              layer.select_byExtendingSelection(true, true);
+          }
+      }
+  }
 };
